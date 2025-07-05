fabeed7e2737d84f2dbc2d731c14ef111e3fbd82
sydneyleeATX <sydney.r.lee04@gmail.com>
2025-07-05 13:40:15 -0500

    Small changes to button css for resiliency in native app

---
+++ android/app/src/main/java/com/leesydney/organizingbuddy/MainActivity.java
@@ -0,0 +1,5 @@
+package com.leesydney.organizingbuddy;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.leesydney.organizingbuddy',
+  appName: 'OrganizeAI',
+  // webDir must point to where built web application files are located
+  webDir: 'out'
+};
+
+export default config;
